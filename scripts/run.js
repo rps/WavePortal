129c22ab95f2a3052974ebfa0b073bd0c7bc8a96
Rich Parrish <richparrishx@gmail.com>
2022-01-07 15:37:59 -0500

    deploy.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,26 @@
 const main = async () => {
+  const [owner, ...people] = await hre.ethers.getSigners();
+  // Grabs the contract and spins up a network(?)
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  // Deploys the smart contract to the network and returns a reference
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
   console.log('Contract deployed to:', waveContract.address);
+  console.log('Contract deployed by: ', owner.address);
+
+  let waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  for (const person of people) {
+    waveTxn = await waveContract.connect(person).wave();
+    await waveTxn.wait();
+  }
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
